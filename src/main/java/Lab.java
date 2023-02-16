12c846c99ec146c532cc3c220d9d9f178ff23db1
melis-a <rampersaudmelisa@gmail.com>
2023-02-16 17:15:31 +0000

    Completed Combine Strings Lab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -8,7 +8,7 @@ public class Lab {
      * @param b the second part of the string to be combined.
      * @return the combination of strings a and b.
      */
-    public String combine(String a, String b){
-        return "";
+    public static String combine(String a, String b){
+        return a + b;
     }
 }
